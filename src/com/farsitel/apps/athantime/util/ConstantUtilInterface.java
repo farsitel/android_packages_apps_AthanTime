25572294db6650567bafdb156af9531379c06b7c
Majid Kalkatechi <majid@farsitel.com>
2011-01-24 11:39:42 +0330

    sumsung release stable

--- src/com/farsitel/apps/athantime/util/ConstantUtilInterface.java
+++ src/com/farsitel/apps/athantime/util/ConstantUtilInterface.java
@@ -1,13 +1,19 @@
 package com.farsitel.apps.athantime.util;
 
-public class ConstantUtil {
-    // Preventing users to instantiate this class
-    private ConstantUtil(){}
+
+public interface ConstantUtilInterface {
+
     // Tag that is used for Log class
     public static final String NAMAZ_LOG_TAG = "namaz";
     public static final String NAMAZ_QIBLA_LOG_TAG = "namaz qibla Manager";
     // Minimum distance or time to update the location of cell phone with GPS
-    public  static final long MIN_LOCATION_TIME = 60;
+    public static final long MIN_LOCATION_TIME = 60;
     public static final long MIN_LOCATION_DISTANCE = 20;
+
+    // public static final Map<Integer, Integer> STATE_VALUE_KEY = new
+    // HashMap<Integer, Integer>();
+    public static final int MENU_HELP = 35;
+    public static final int MENU_PREFS = 34;
+    public static final int MENU_ABOUT = 33;
 
 }
