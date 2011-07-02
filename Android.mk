5d165395d22a4bce080ba6023c91a1746bad59c1
Reza Mohammadi <reza@farsitel.com>
2011-07-02 11:10:19 +0430

    Added adad.jar to the Make file

--- Android.mk
+++ Android.mk
@@ -20,6 +20,8 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_STATIC_JAVA_LIBRARIES := libadad
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
 LOCAL_SDK_VERSION := current
@@ -30,6 +32,7 @@ include $(BUILD_PACKAGE)
 ##################################################
 include $(CLEAR_VARS)
 
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := libadad:lib/adad.jar
 include $(BUILD_MULTI_PREBUILT)
 
 # Use the folloing include to make our test apk.
